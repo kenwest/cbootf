b3b0ea650879caf79444d9ac4dfaa6fda04cbac7
Ken West <ken.west@internode.on.net>
2015-09-24 20:42:41 +1000

    Hide the page header on the city pages, using PHP rather than CSS. The CSS was breaking some Views pages and causing them to display without a page header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -136,7 +136,7 @@
         <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
         <a id="main-content"></a>
         <?php print render($title_prefix); ?>
-        <?php if (!empty($title)): ?>
+        <?php if (!empty($title) && !preg_match('!^city/[a-z]+$!i', check_plain(current_path()))): ?>
           <h1 class="page-header"><?php print $title; ?></h1>
         <?php endif; ?>
         <?php print render($title_suffix); ?>
