fccc0fa1226ef1556dd586c20694801bc30e6d7a
Ken West <ken.west@internode.on.net>
2016-05-01 18:28:16 +1000

    Make the 'link-button' style less specific so it works in the HTML editor

--- css/style.sh
+++ css/style.sh
@@ -2,7 +2,7 @@
 #
 # Generate the CSS files
 
-cd `dirname $0`
+cd $(dirname $0)
 
 lessc ../less/style.less > style.css
 curl -X POST -s --data-urlencode 'input@style.css' http://cssminifier.com/raw > style.min.css
