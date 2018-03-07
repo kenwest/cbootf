aa813d1a27837540ffd7294a61f843fceaffdc22
kenwest <ken.west@internode.on.net>
2018-03-07 17:50:04 +1100

    Adopt the reaching.city style of header

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -75,43 +75,72 @@
 ?>
 <header id="navbar" role="banner" class="<?php print $navbar_classes; ?>">
   <div class="container">
-    <div class="row">
-      <div class="navbar-header">
-        <?php if ($logo): ?>
-        <a class="logo navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+    <div class="navbar-header">
+      <?php if ($logo): ?>
+        <a class="logo horizontal navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
           <object data="<?php print $logo; ?>" type="image/svg+xml">
             <img src="<?php print str_replace('.svg', '.png', $logo); ?>" alt="<?php print t('Home'); ?>" />
           </object>
         </a>
-        <?php endif; ?>
+        <a class="logo vertical navbar-btn pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+          <object data="<?php print str_replace('logo.', 'vertical-logo.', $logo); ?>" type="image/svg+xml">
+            <img src="<?php print str_replace('logo.svg', 'vertical-logo.png', $logo); ?>" alt="<?php print t('Home'); ?>" />
+          </object>
+        </a>
+      <?php endif; ?>
 
+      <div class="navbar-toggles">
         <?php if (!empty($site_name)): ?>
-        <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
+          <a class="name navbar-brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>"><?php print $site_name; ?></a>
         <?php endif; ?>
 
-        <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
-        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-          <span class="sr-only">Toggle navigation</span>
-          <i class="fa fa-fw fa-bars"></i>
-        </button>
-      </div>
+        <?php if (!empty($primary_nav)): ?>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-parent="#navbar-collapse-group" href="#navbar-collapse-primary">
+            <span class="sr-only"><?php print t('Toggle primary navigation'); ?></span>
+            <i class="fa fa-fw fa-bars"></i>
+          </button>
+        <?php endif; ?>
 
-      <?php if (!empty($primary_nav) || !empty($secondary_nav) || !empty($page['navigation'])): ?>
-        <div class="navbar-collapse collapse">
-          <nav role="navigation">
-            <?php if (!empty($primary_nav)): ?>
-              <?php print render($primary_nav); ?>
-            <?php endif; ?>
-            <?php if (!empty($secondary_nav)): ?>
-              <?php print render($secondary_nav); ?>
-            <?php endif; ?>
-            <?php if (!empty($page['navigation'])): ?>
-              <?php print render($page['navigation']); ?>
-            <?php endif; ?>
+        <?php if (!empty($secondary_nav)): ?>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-parent="#navbar-collapse-group" href="#navbar-collapse-secondary">
+            <span class="sr-only"><?php print t('Toggle city navigation'); ?></span>
+            <i class="fa fa-fw fa-map-marker"></i>
+          </button>
+        <?php endif; ?>
+
+        <?php if (!empty($page['navigation'])): ?>
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-parent="#navbar-collapse-group" href="#navbar-collapse-search">
+            <span class="sr-only"><?php print t('Toggle search'); ?></span>
+            <i class="fa fa-fw fa-search"></i>
+          </button>
+        <?php endif; ?>
+      </div>
+    </div>
+
+    <div class="panel-group navbar-collapse" id="navbar-collapse-group">
+      <div class="panel">
+        <div class="panel-collapse collapse" id="navbar-collapse-primary">
+          <nav class="panel-body" role="navigation">
+            <?php print render($primary_nav); ?>
           </nav>
         </div>
-      <?php endif; ?>
+      </div>
+      <div class="panel">
+        <div class="panel-collapse collapse" id="navbar-collapse-secondary">
+          <nav class="panel-body" role="navigation">
+            <?php print render($secondary_nav); ?>
+          </nav>
+        </div>
+      </div>
+      <div class="panel">
+        <div class="panel-collapse collapse" id="navbar-collapse-search">
+          <nav class="panel-body" role="navigation">
+            <?php print render($page['navigation']); ?>
+          </nav>
+        </div>
+      </div>
     </div>
+
   </div>
 </header>
 
